35681242f63cf4da40f547295d98458af8d98dbe
namhyun <namhyun3418@gmail.com>
2024-02-13 05:43:48 +0900

    모델

---
+++ planner/models/users.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from beanie import Document, Link
+from models.events import Event
+
+class User(Document):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Settings:
+        name = "users"
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
